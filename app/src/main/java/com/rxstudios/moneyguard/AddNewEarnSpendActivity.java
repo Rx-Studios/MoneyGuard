ea451139630def8f6ce8eb4dab0f4983b3dee6c7
xSmair <simonmair01@gmx.at>
2020-09-21 14:50:20 +0200

    ...

---
+++ app/src/main/java/com/rxstudios/moneyguard/AddNewEarnSpendActivity.java
@@ -0,0 +1,14 @@
+package com.rxstudios.moneyguard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNewEarnSpendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_earn_spend);
+    }
+}
--- app/src/main/java/com/rxstudios/moneyguard/MainActivity.java
+++ app/src/main/java/com/rxstudios/moneyguard/MainActivity.java
@@ -1,9 +1,12 @@
 package com.rxstudios.moneyguard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.rxstudios.moneyguard.beans.EarnSpend;
 
 import java.util.ArrayList;
@@ -12,6 +15,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     List<EarnSpend> earnSpendList = new ArrayList<>();
+    FloatingActionButton addNewEarnSpendButton;
 
     public List<EarnSpend> getEarnSpendList() {
         return earnSpendList;
@@ -23,5 +27,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        findViewById(R.id.addNewEarnSpendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddNewEarnSpendActivity.class);
+                startActivityForResult(intent, 001);
+            }
+        });
     }
 }
